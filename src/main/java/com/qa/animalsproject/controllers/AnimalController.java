03bb964af32f6dd39cd4d7a48fdb201fb682d6dc
Kate-ua <lesnaya_k@hotmail.com>
2022-07-12 21:50:28 +0100

    dependency injection added

--- src/main/java/com/qa/animalsproject/controllers/AnimalController.java
+++ src/main/java/com/qa/animalsproject/controllers/AnimalController.java
@@ -5,35 +5,40 @@ import java.util.List;
 @RestController
 @RestMapping("/animals")
 public class AnimalController {
-	
-	
 
-@GetMapping("/readAll")
-public List<Animals>readAll(){
-	
-}
+	private AnimalsController services;
 
-@GetMapping("/readById/{id}")
-public Animals readbyId(@PathVariable long id) {
-	
-}
+	public AnimalsController(AnimalsService service) {
+		this.service=service;
+	}
 
-@GetMapping("readByType/{type}")
-public List<Animals> readByType(@PathVariable String type){
-	
-}
+	@GetMapping("/readAll")
+	public List<Animals> readAll() {
 
-@PostMapping("/create")
-public Animals create(@RequestBody Animals animals) {
-	
-}
+	}
 
-@PutMapping("/update/{id}")
-public Animals updateAnimals(@PathVariable long id, @RequestBody Animals animals) {
-	
-}
-@DeleteMapping("/delete/{id}")
-public boolean delete(@PathVariable long id) {
-	
-}
+	@GetMapping("/readById/{id}")
+	public Animals readbyId(@PathVariable long id) {
+
+	}
+
+	@GetMapping("readByType/{type}")
+	public List<Animals> readByType(@PathVariable String type) {
+
+	}
+
+	@PostMapping("/create")
+	public Animals create(@RequestBody Animals animals) {
+
+	}
+
+	@PutMapping("/update/{id}")
+	public Animals updateAnimals(@PathVariable long id, @RequestBody Animals animals) {
+
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public boolean delete(@PathVariable long id) {
+
+	}
 }
