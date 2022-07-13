4cb13f0d4091ea5dd33f938aa3f82ba8938a598f
Kate-ua <lesnaya_k@hotmail.com>
2022-07-13 16:32:50 +0100

    imports added to test controller

--- src/test/java/com/qa/animalsproject/controllers/AnimalControllerTest.java
+++ src/test/java/com/qa/animalsproject/controllers/AnimalControllerTest.java
@@ -1,6 +1,7 @@
 package com.qa.animalsproject.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
