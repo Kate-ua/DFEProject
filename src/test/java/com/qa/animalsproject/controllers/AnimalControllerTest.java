4a2e656411fddabc7bda7233d317e25cf0a8b407
Kate-ua <lesnaya_k@hotmail.com>
2022-07-13 16:20:58 +0100

    controllerTest added

--- src/test/java/com/qa/animalsproject/controllers/AnimalControllerTest.java
+++ src/test/java/com/qa/animalsproject/controllers/AnimalControllerTest.java
@@ -1,5 +1,47 @@
 package com.qa.animalsproject.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.animalsproject.entities.Animals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class AnimalControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	@Test
+	public void ReadAllTest() throws Exception {
+		List<Animals>output = new ArrayList<>();
+		Animals entry = new Animals (1L, "koala", "open forest", 11, 2);
+		output.add(entry);
+		
+		String outputAsJSON = mapper.writeValueAsString(output);
+		
+		mvc.perform(get("/supplyer/readAll")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(outputAsJSON));
+		
+		
+	}
 
 }
