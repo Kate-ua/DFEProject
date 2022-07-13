bcd2c0f005520b39817e139b466ce31f08a46f74
Kate-ua <lesnaya_k@hotmail.com>
2022-07-13 09:20:58 +0100

    imports added

--- src/main/java/com/qa/animalsproject/controllers/AnimalsController.java
+++ src/main/java/com/qa/animalsproject/controllers/AnimalsController.java
@@ -2,14 +2,23 @@ package com.qa.animalsproject.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.animalsproject.entities.Animals;
+import com.qa.animalsproject.services.AnimalsService;
 
 @RestController
 @RequestMapping("/animals")
 public class AnimalsController {
 
-	private AnimalsService services;
+	private AnimalsService service;
 
 	public AnimalsController(AnimalsService service) {
 		this.service=service;
@@ -35,7 +44,7 @@ public class AnimalsController {
 
 	@PostMapping("/create")
 	public Animals create(@RequestBody Animals animals) {
-		return this.service.update(id, animals);
+		return this.service.create(animals);
 
 	}
 
